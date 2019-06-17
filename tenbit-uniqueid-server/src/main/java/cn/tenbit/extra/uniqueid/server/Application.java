0ebf7046e5dce18a26f8edd9f8a7b271ff97d49c
bangquan.qian <chainz@foxmail.com>
2019-06-17 17:31:33 +0800

    commit

---
+++ tenbit-uniqueid-server/src/main/java/cn/tenbit/extra/uniqueid/server/Application.java
@@ -0,0 +1,12 @@
+package cn.tenbit.extra.uniqueid.server;
+
+/**
+ * @Author bangquan.qian
+ * @Date 2019-06-17 17:24
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
