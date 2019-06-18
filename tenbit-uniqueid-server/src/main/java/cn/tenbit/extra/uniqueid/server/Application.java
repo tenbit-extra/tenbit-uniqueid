56832f1d8d53c293dc72bb051a17cead2e73317b
bangquan.qian <chainz@foxmail.com>
2019-06-18 13:23:02 +0800

    commit

--- tenbit-uniqueid-server/src/main/java/cn/tenbit/extra/uniqueid/server/Application.java
+++ tenbit-uniqueid-server/src/main/java/cn/tenbit/extra/uniqueid/server/Application.java
@@ -1,12 +1,16 @@
 package cn.tenbit.extra.uniqueid.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Author bangquan.qian
  * @Date 2019-06-17 17:24
  */
+@SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Application.class, args);
     }
 }
